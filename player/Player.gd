a6321b45587625e5ca54b56f47f6e918a2631074
ballsballsballsballsballsballsballsball <sorenzio69@gmail.com>
2022-03-25 02:02:03 -0500

    oh my god explosion

--- player/Player.gd
+++ player/Player.gd
@@ -2,16 +2,18 @@ extends KinematicBody2D
 
 onready var animated_sprite : AnimatedSprite = $AnimatedSprite
 onready var victory_text : RichTextLabel = $"Victory text"
+onready var explosion : Particles2D = $Explosion
 
 export var movement_speed = 250.0
 
 func _physics_process(delta):
-	var motion : = Vector2()
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	update_animation(motion)
-	move_and_slide(motion.normalized() * movement_speed, Vector2())
+	if not victory_text.visible:
+		var motion : = Vector2()
+		motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+		motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+		
+		update_animation(motion)
+		move_and_slide(motion.normalized() * movement_speed, Vector2())
 	
 func update_animation(motion: Vector2):
 	var animation = 'idle'
@@ -29,4 +31,7 @@ func update_animation(motion: Vector2):
 
 
 func _on_Goal_body_entered(body):
+	explosion.show()
+	explosion.emitting = true
+	animated_sprite.play('idle')
 	$"Victory text".show()
